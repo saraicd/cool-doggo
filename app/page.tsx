376d38e8da2305ed79b22555edb6916491118972
saradomi <115647886+saradomi@users.noreply.github.com>
2025-02-05 11:06:20 +0000

    Added a tooltip to logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Footer from "./Footer";
 import Lamp from "./Lamp";
 import CoolDoggo from "./../public/cool-doggo_square-full.svg";
@@ -8,10 +7,13 @@ export default function Home() {
   return (<>
       <Lamp/>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="image" style={{ width: "150px"}}>
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative inline-block group">
+          <div className="image w-[150px] transform transition-transform duration-300 ease-in-out group-hover:scale-110">
             <CoolDoggo  />
           </div>
+          <span className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-white text-sm bg-black rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            Available soon
+          </span>
         </main>
         <Footer/>
       </div>
